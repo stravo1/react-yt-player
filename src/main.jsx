ef4effe34e5aa35b9e6a3cde7af152c81e896f78
tab-a7 <stravoone@gmail.com>
2022-08-01 12:12:51 +0530

    initial: initial commit
    Player.jsx: controls and logic for the youtube player
    index.css: styling for tailwind and the range input slider
    main.jsx: the video id for the player

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Player from './Player'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+		<div className="p-8">
+			<Player videoId={"dQw4w9WgXcQ"}/>
+		</div>
+  </React.StrictMode>
+)
